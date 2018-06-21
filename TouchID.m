f727705c2984071935c5ce839111853a96f90cf6
Algirdas Gridziuska <19371064+algirdas1@users.noreply.github.com>
2018-06-21 16:13:28 +0200

    exposed error codes for ios

--- TouchID.m
+++ TouchID.m
@@ -2,15 +2,52 @@
 #import <React/RCTUtils.h>
 #import "React/RCTConvert.h"
 
+int RCTTouchIDNotSupported = 100;
+
 @implementation TouchID
 
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (NSDictionary *)constantsToExport
+{
+    NSMutableDictionary * dict = [@{ @"LAErrorAuthenticationFailed": @(LAErrorAuthenticationFailed),
+                             @"LAErrorUserCancel": @(LAErrorUserCancel),
+                             @"LAErrorUserFallback": @(LAErrorUserFallback),
+                             @"LAErrorSystemCancel": @(LAErrorSystemCancel),
+                             @"LAErrorPasscodeNotSet": @(LAErrorPasscodeNotSet),
+                             @"LAErrorAppCancel": @(LAErrorAppCancel),
+                             @"LAErrorInvalidContext": @(LAErrorInvalidContext),
+                             @"LAErrorNotInteractive": @(LAErrorNotInteractive),
+                                     @"RCTTouchIDNotSupported": @(RCTTouchIDNotSupported)
+                             } mutableCopy];
+    
+    if (@available(iOS 11.0, *)) {
+        [dict addEntriesFromDictionary:@{
+                                        @"LAErrorBiometryNotAvailable": @(LAErrorBiometryNotAvailable),
+                                        @"LAErrorBiometryNotEnrolled": @(LAErrorBiometryNotEnrolled),
+                                        @"LAErrorBiometryLockout": @(LAErrorBiometryLockout),
+                                        }];
+    } else {
+        [dict addEntriesFromDictionary:@{
+                                        @"LAErrorBiometryNotAvailable": @(LAErrorTouchIDNotAvailable),
+                                        @"LAErrorBiometryNotEnrolled": @(LAErrorTouchIDNotEnrolled),
+                                        @"LAErrorBiometryLockout": @(LAErrorTouchIDLockout),
+                                        }];
+    }
+    return dict;
+}
+
 RCT_EXPORT_MODULE();
+
 
 RCT_EXPORT_METHOD(isSupported: (RCTResponseSenderBlock)callback)
 {
     LAContext *context = [[LAContext alloc] init];
     NSError *error;
-
+    
     if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
         callback(@[[NSNull null], [self getBiometryType:context]]);
         // Device does not support TouchID
@@ -26,12 +63,14 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
 {
     LAContext *context = [[LAContext alloc] init];
     NSError *error;
-
+    
     if (RCTNilIfNull([options objectForKey:@"fallbackLabel"]) != nil) {
-        NSString *fallbackLabel = [RCTConvert NSString:options[@"fallbackLabel"]];   
+        NSString *fallbackLabel = [RCTConvert NSString:options[@"fallbackLabel"]];
         context.localizedFallbackTitle = fallbackLabel;
+    } else {
+        context.localizedFallbackTitle = @"";
     }
-
+    
     // Device has TouchID
     if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
         // Attempt Authentification
@@ -42,52 +81,14 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
              if (success) { // Authentication Successful
                  callback(@[[NSNull null], @"Authenticated with Touch ID."]);
              } else if (error) { // Authentication Error
-                 NSString *errorReason;
-
-                 switch (error.code) {
-                     case LAErrorAuthenticationFailed:
-                         errorReason = @"LAErrorAuthenticationFailed";
-                         break;
-
-                     case LAErrorUserCancel:
-                         errorReason = @"LAErrorUserCancel";
-                         break;
-
-                     case LAErrorUserFallback:
-                         errorReason = @"LAErrorUserFallback";
-                         break;
-
-                     case LAErrorSystemCancel:
-                         errorReason = @"LAErrorSystemCancel";
-                         break;
-
-                     case LAErrorPasscodeNotSet:
-                         errorReason = @"LAErrorPasscodeNotSet";
-                         break;
-
-                     case LAErrorTouchIDNotAvailable:
-                         errorReason = @"LAErrorTouchIDNotAvailable";
-                         break;
-
-                     case LAErrorTouchIDNotEnrolled:
-                         errorReason = @"LAErrorTouchIDNotEnrolled";
-                         break;
-
-                     default:
-                         errorReason = @"RCTTouchIDUnknownError";
-                         break;
-                 }
-
-                 NSLog(@"Authentication failed: %@", errorReason);
-                 callback(@[RCTMakeError(errorReason, nil, nil)]);
+                 callback(@[RCTMakeError(error.description, nil, @{ @"code": @(error.code) })]);
              } else { // Authentication Failure
-                 callback(@[RCTMakeError(@"LAErrorAuthenticationFailed", nil, nil)]);
+                 callback(@[RCTMakeError(@"failed", nil, @{ @"code": @(LAErrorAuthenticationFailed) })]);
              }
          }];
-
         // Device does not support TouchID
     } else {
-        callback(@[RCTMakeError(@"RCTTouchIDNotSupported", nil, nil)]);
+        callback(@[RCTMakeError(@"RCTTouchIDNotSupported", nil,  @{ @"code": @(RCTTouchIDNotSupported) })]);
         return;
     }
 }
@@ -97,7 +98,7 @@ RCT_EXPORT_METHOD(authenticate: (NSString *)reason
     if (@available(iOS 11, *)) {
         return (context.biometryType == LABiometryTypeFaceID) ? @"FaceID" : @"TouchID";
     }
-
+    
     return @"TouchID";
 }
 
